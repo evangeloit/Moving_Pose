9d93b8ff2f4740c39cbc2736643ea3ff8e13d653
Evangelos Germenis <evangeloit@gmail.com>
2018-09-05 22:00:39 +0300

    Master Branch Initial Commit

---
+++ MVP_test1.py
@@ -0,0 +1,75 @@
+import PythonModel3dTracker.PythonModelTracker.TrackingResults.ModelTrackingResults as Mtr
+import numpy as np
+import json
+
+
+input_dir = "/home/evangeloit/Desktop/GitBlit_Master/PythonModel3dTracker/Data/rs/Human_tracking/alex_far_01_ldm.json"
+model_name = "mh_body_male_custom"
+
+##### Implementation with ModelTrackingResults scripts #####
+
+# results = Mtr.ModelTrackingResults()
+# results.load(input_dir)
+# ldm3d = results.landmarks
+
+# frames = -1
+# for i in ldm3d:
+#     print(ldm3d[i])
+#     frames = frames + 1
+
+# print(frames)
+
+##### My implementation #####
+with open(input_dir) as f:
+    data = json.load(f)
+
+LandSize = len(data['landmarks']['0'][model_name])
+
+if LandSize == 14:
+    print("You have :", LandSize, "Landmarks")
+else:
+    print('No Landmarks to Import')
+
+
+points = []
+for i in range(0,len(data['landmarks'])):
+    strnum = str(i)
+    # print(strnum)
+    for item in data['landmarks'][strnum][model_name]:
+            points.append(item)
+            # print(point3d)
+
+#### Convert to numpy array for calculations ###
+point3d = np.array(points)
+point3d = np.transpose(point3d)
+
+#### Normalization of 3d points with Gaussian filter [5 1] ####
+# GFpoint3d = gaussian_filter(point3d, sigma=1)
+
+
+# print(len(point3d))
+# print(point3d[0:,14]-point3d[0:,28])
+
+
+sz = point3d.shape
+# print(sz[1])
+t1 = LandSize
+t2 = 2*LandSize
+StartFrame = 2*LandSize # Start from 3rd Frame's 3D points
+count = 0
+
+for col in range(StartFrame, sz[1] - StartFrame):
+    for row in range(0, sz[0]):
+        Pt0 = point3d[row, col]
+        Pt1 = point3d[row, col + t1]
+        Pt2 = point3d[row, col + t2]
+        PtMinus1 = point3d[row, col - t2]
+        PtMinus2 = point3d[row, col - t2]
+
+
+
+
+# how do i dynamically allocate the arrays
+# Save Feature data into json...
+# Calculate the First and Second Derivative
+# Build the feature vector
