620bd28bb04df6c4a6882ab3bd77c1520df9cecb
Evangelos Germenis <evangeloit@gmail.com>
2018-11-01 15:38:18 +0200

    New MP_Conf_Matrix script + heatmap script

--- MP_conf_matrix.py
+++ MP_conf_matrix.py
@@ -13,16 +13,18 @@ import os
 import MP_tools2 as mpt
 import dpcore #dtw c
 import matplotlib.pyplot as plt
+from heatmap import heatmap
+from heatmap import annotate_heatmap
 
 # Controllers
 
 # dataset = ['mhad_s01_a04', 'mhad_s02_a04', 'mhad_s03_a04','mhad_s04_a04'\
 #             ,'mhad_s05_a04', 'mhad_s06_a04', 'mhad_s07_a04','mhad_s08_a04','mhad_s09_a01','mhad_s10_a04', 'mhad_s11_a04', 'mhad_s12_a04']
-dataset_s1 = ['mhad_s01_a02', 'mhad_s01_a03','mhad_s01_a04',\
-          'mhad_s01_a05', 'mhad_s01_a06',\
-          'mhad_s01_a09',\
+dataset_s1 = ['mhad_s01_a01','mhad_s01_a02', 'mhad_s01_a03','mhad_s01_a04',\
+          'mhad_s01_a05', 'mhad_s01_a06','mhad_s01_a07','mhad_s01_a08',\
+          'mhad_s01_a09','mhad_s01_a10','mhad_s01_a11',\
             'mhad_s06_a02', 'mhad_s06_a03','mhad_s06_a04', 'mhad_s06_a05',\
-          'mhad_s06_a06','mhad_s06_a09']
+          'mhad_s06_a06','mhad_s06_a07','mhad_s06_a08','mhad_s06_a09','mhad_s06_a10','mhad_s06_a11']
 
 # dataset_s6 = ['mhad_s06_a01','mhad_s06_a02', 'mhad_s06_a03','mhad_s06_a04', 'mhad_s06_a05',\
 #           'mhad_s06_a06','mhad_s06_a07','mhad_s06_a08','mhad_s06_a09', 'mhad_s06_a10', 'mhad_s06_a11']
@@ -47,7 +49,7 @@ savefig_comp = os.getcwd() + "/plots/conf_matrix/MP_comp_mat/"
 savefig_dtw = os.getcwd() + "/plots/conf_matrix/dtw_res_conf/"
 
 # sflag =  0 : Turn off plots , 1: save figures to path
-sflag = 1
+sflag = 0
 
 FV_new = []
 
@@ -60,8 +62,8 @@ for name in dataset_s1:
     ## Load data from Json ##
     dataPoints, dataLim = mpt.load_data(input_dir, dataset_dir)
 
-    init_frame = 0#dataLim['limits'][0]
-    last_frame = 190#dataLim['limits'][1]
+    init_frame = dataLim['limits'][0]
+    last_frame = dataLim['limits'][1]
 
     ##### Create 3D points array #####
 
@@ -102,7 +104,30 @@ for subject1 in range(0, len(dataset_s1)/2):
         mpt.DistMatPlot(Y, savefig_dtw, q, p, name=dataset_s1[subject1]+"||"+dataset_s1[subject6+(len(dataset_s1)/2)], flag='DTW', save_flag=sflag)
         #Scores of DTW for every subject
         print(len(Y))
-        score[subject1][subject6] = C[-1, -1]/len(Y)
+        score[subject1][subject6] = (C[-1, -1]/(Y.shape[0]+Y.shape[1]))/100
+
+# Confusion Matrix
+
+# actionsS1 = ["A02","A03","A04","A05","A06","A09"]
+# actionsS6 = ["A02","A03","A04","A05","A06","A09"]
+
+actionsS1 = ["A01","A02","A03","A04","A05","A06","A07","A08","A09","A10","A11"]
+actionsS6 = ["A01","A02","A03","A04","A05","A06","A07","A08","A09","A10","A11"]
+
+fig, ax = plt.subplots()
+ax.set_xlabel('S06')
+ax.set_ylabel('S01')
+ax.xaxis.set_label_position('top')
+
+im, cbar = heatmap(score, actionsS1, actionsS6,ax=ax,
+                   cmap=None, cbarlabel="Score")
+
+texts = annotate_heatmap(im, valfmt="{x:.1f} ")
+
+plt.rcParams.update({'font.size': 13})
+
+fig.tight_layout()
+plt.show()
 
 print()
 
