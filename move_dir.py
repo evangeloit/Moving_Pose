09e6bae393c470bd2ec68c4ce060ef6bf2302345
Evangelos Germenis <evangeloit@gmail.com>
2018-11-13 04:02:05 +0200

    move_dir.py update

--- move_dir.py
+++ move_dir.py
@@ -10,15 +10,14 @@ import re
 import sys
 
 
-
-# path = '/home/evangeloit/Desktop/GitBlit_Master/PythonModel3dTracker/Data/data/s01_a01/'
-path = '/home/evangeloit/Desktop/GitBlit_Master/PythonModel3dTracker/Data/data/S009'
+path = '/home/evangeloit/Desktop/GitBlit_Master/PythonModel3dTracker/S009'
 
 subdir_names = ['R01']
 filelist = os.listdir(path)
 src = []
 dstn = []
-# # Check for directories with images.
+
+#Make source and destenation dirs
 for f in filelist:
     dest = os.path.join(path, f)
     dstn.append(dest)
@@ -26,12 +25,17 @@ for f in filelist:
         source =os.path.join(dest,file)
         for n in subdir_names:
             if n in file: src.append(source)
-
+#Move Files
+# print(src)
 for num in range(0,len(src)):
     for filename in listdir(join(dstn[num], subdir_names[0])):
-        # print(src[num])
+        # print(filename)
         move(join(src[num],filename),join(dstn[num],''))
-        # sub_folders_pathname = src[num]
-        # sub_folders_list = glob.glob(sub_folder_pathname)
-        # for sub_folder in sub_folders_list:
-        #     shutil.rmtree(sub_folder)
+
+#Remove Empty dirs
+for num2 in range(0,len(src)):
+    sub_folders_pathname = src[num2]
+    sub_folders_list = glob.glob(sub_folders_pathname)
+    for sub_folder in sub_folders_list:
+    # print(sub_folder)
+        shutil.rmtree(sub_folder)
