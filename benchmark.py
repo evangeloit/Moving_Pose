9dd723cd445bd165c6e988fb9bd630db9c5c993c
Evangelos Germenis <evangeloit@gmail.com>
2019-01-08 20:52:13 +0200

    Best params _ High noise test2/ Parameters for the highest noise.

--- benchmark.py
+++ benchmark.py
@@ -35,25 +35,25 @@ def benchmark(train, filter_func, test, k, metric, age_uncertainty_in_frames=0):
 
 database = np.load("db_frame_subject_action_age.npy")
 
-train, test = databaseModify.reduceDatabase(database, 10000, 150)
+train, test = databaseModify.reduceDatabase(database, 10000, 400)
+#
+wvec = wd.wvector(0.4, 0.64, 0.3)
 
-#
-# filter = functools.partial(db_filter_window.db_window, 100)
-#
-# print(benchmark(train, filter, test, 6, dst.euclidean))
-# print(benchmark(train, filter, test, 6, dst.euclidean, 5))
-# print(benchmark(train, filter, test, 6, dst.euclidean, 10))
-# print(benchmark(train, filter, test, 6, dst.euclidean, 15))
-#
-#
-# exit()
+metric = functools.partial(wd.wdistance, wvec)
 
+filter = functools.partial(db_filter_window.db_window, 200)
+
+# start_time = time.time()
+result = benchmark(train, filter, test, 3, metric, 15)
+
+print(result)
+exit()
+
+# BEST params for mhad with windowSize:200 and noise level :15
+# [Accuracy0.91, wPos : 0.4 ,wVel : 0.64, wAcc : 0.3, k : 3]
 
 results = []
 # iRow = 0
-
-#
-
 # age_uncertainty_in_frames = [5, 10, 15]
 #
 # for uncertainty in [0, 5, 10, 15]:
@@ -83,10 +83,10 @@ for wpos in range(1, 11):
                     # print("--- %s seconds ---" % (time.time() - start_time))
                     # exit()
 
-                    res = [result[0], wp, wvel, wacc, k]
+                    res = [result[0], wp, wv, wa, k]
                     results.append(res)
 
 
 results = np.array(results)
-np.save('best_params_for_High_noise_fixed_size_200.npy', results)
+np.save('best_params_for_High_noise_test2.npy', results)
 print()
