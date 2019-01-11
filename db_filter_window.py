2ace50d5361c11339b255ce6bd6ec4de100b80b4
Evangelos Germenis <evangeloit@gmail.com>
2019-01-12 00:37:57 +0200

    Added functionality of relattive window size. Function db_window function now accepts a WindowsSizeRelative input parameter in range [0 -1](percentage) and computes the WindowSizeAbsolute value based on the sequence total length.
    WidnowsSizeAbsolute = floor(WindowSizeRelative * Total_length_of_Sequene)

--- db_filter_window.py
+++ db_filter_window.py
@@ -30,3 +30,19 @@ def db_window(windowSize, train, age):
 
     return np.array(keep_frames)
 
+
+def db_window_relative(windowSizeRelative, train, age):
+
+    keep_frames = []
+
+    for iframe in range(0, train.shape[0]):
+
+        windowSizeAbs = int(windowSizeRelative * train[iframe][5])
+
+        window_range = [age - windowSizeAbs / 2, age + (windowSizeAbs + 1) / 2]
+
+        if  window_range[0] <= train[iframe][3] & train[iframe][3] < window_range[1]:
+
+            keep_frames.append(train[iframe])
+
+    return np.array(keep_frames)
